b17db2ab075851818bd6b00bdac13ea814d47680
mkocabas <muhammed.kocabas@metu.edu.tr>
2019-07-11 19:28:03 +0200

    fix

--- demo.sh
+++ demo.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-export DATADIR=/home/mkocabas/developments/soccerontable/barcelona
+export DATADIR=/home/mkocabas/developments/soccerontable/data/galatasaray
 export DETECTRON=/home/mkocabas/developments/detectron
 export SOCCERCODE=/home/mkocabas/developments/soccerontable
 export OPENPOSEDIR=/home/mkocabas/developments/openpose
@@ -7,10 +7,11 @@ export OMP_NUM_THREADS=8
 export MODELPATH=/home/mkocabas/developments/soccerontable/model.pth
 
 cd $DETECTRON
+
 python tools/infer_subimages.py --cfg configs/12_2017_baselines/e2e_mask_rcnn_R-101-FPN_2x.yaml \
                                 --output-dir $DATADIR/detectron \
                                 --image-ext jpg \
-                                --wts https://s3-us-west-2.amazonaws.com/detectron/35861858/12_2017_baselines/e2e_mask_rcnn_R-101-FPN_2x.yaml.02_32_51.SgT4y1cO/output/train/coco_2014_train:coco_2014_valminusminival/generalized_rcnn/model_final.pkl \
+                                --wts https://dl.fbaipublicfiles.com/detectron/35861858/12_2017_baselines/e2e_mask_rcnn_R-101-FPN_2x.yaml.02_32_51.SgT4y1cO/output/train/coco_2014_train:coco_2014_valminusminival/generalized_rcnn/model_final.pkl \
                                 $DATADIR/images/
 
 cd $SOCCERCODE
